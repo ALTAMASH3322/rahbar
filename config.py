b6295745fa311daf55534ab2391e724085eebe51
ALTAMASH3322 <63273631+ALTAMASH3322@users.noreply.github.com>
2025-06-28 20:27:43 +0530

    updated database
    database

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hello'
 
     # Database configuration
-    MYSQL_USER = 'root'
-    #MYSQL_PASSWORD = 'Spidey@321654'
-    #MYSQL_HOST = '139.59.3.48'
-    MYSQL_PASSWORD = ''
-    MYSQL_HOST = '127.0.0.1'
+    MYSQL_USER = 'alta'
+    MYSQL_PASSWORD = 'hello'
+    MYSQL_HOST = '194.195.87.118'
+    #MYSQL_PASSWORD = ''
+    #MYSQL_HOST = '127.0.0.1'
     MYSQL_DB = 'rahbar'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
